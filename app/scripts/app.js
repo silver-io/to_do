ae1846f951fde3a6a628e73f9d2b8b36c15405ad
javier <js@silveriofunk.com>
2014-09-24 16:59:57 +0100

    added local storage

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,19 +16,17 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ui.sortable'
+    'ui.sortable',
+    'LocalStorageModule'
   ])
-  .config(function ($routeProvider) {
+  .config(['localStorageServiceProvider', '$routeProvider', function(localStorageServiceProvider, $routeProvider){
+    localStorageServiceProvider.setPrefix('ls');
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
       .otherwise({
         redirectTo: '/'
       });
-  });
+  }]);
